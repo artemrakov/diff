382952434962723c57a592cd8b1a464e66024354
Artem Rakov <scoff1k@gmail.com>
2020-03-12 14:21:57 -0400

    refactored

--- src/diff.js
+++ src/diff.js
@@ -1,34 +1,33 @@
 import _ from 'lodash';
 
 const diff = (before, after) => {
-  const buildMethods = [
-    {
-      condition: (key) => before[key] === after[key],
-      getNode: (key) => ({ key, value: before[key], type: 'unchanged' }),
-    },
-    {
-      condition: (key) => _.isObject(before[key]) && _.isObject(after[key]),
-      getNode: (key) => ({ key, children: diff(before[key], after[key]), type: 'nested' }),
-    },
-    {
-      condition: (key) => _.has(before, key) && _.has(after, key),
-      getNode: (key) => ({ key, beforeValue: before[key], afterValue: after[key], type: "changed" })
-    },
-    {
-      condition: (key) => _.has(after, key),
-      getNode: (key) => ({ key, value: after[key], type: 'added' }),
-    },
-    {
-      condition: (key) => _.has(before, key),
-      getNode: (key) => ({ key, value: before[key], type: 'removed' }),
-    },
-  ];
+  const buildNode = (key) => {
+    const beforeValue = before[key];
+    const afterValue = after[key];
+
+    if (beforeValue === afterValue) {
+      return { key, value: beforeValue, type: 'unchanged' };
+    }
+
+    if (_.isObject(beforeValue) && _.isObject(afterValue)) {
+      return { key, children: diff(beforeValue, afterValue), type: 'nested' }
+    }
+
+    if (_.has(before, key) && _.has(after, key)) {
+      return { key, beforeValue, afterValue, type: 'changed' }
+    };
+
+    if (_.has(after, key)) {
+      return { key, value: afterValue, type: 'added' };
+    }
+
+    return { key, value: beforeValue, type: 'removed' };
+  };
 
   const unionKeys = _.union(_.keys(before), _.keys(after));
 
   return unionKeys.reduce((acc, key) => {
-    const { getNode } = buildMethods.find(({ condition }) => condition(key));
-    return [...acc, getNode(key)];
+    return [...acc, buildNode(key)];
   }, []);
 };
 
