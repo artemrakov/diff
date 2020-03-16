3638ec692de1ea7e1dd3fc04e952975ad4c184c4
Artem Rakov <scoff1k@gmail.com>
2020-03-16 17:37:19 -0400

    пофиксил после стрима кирилла

--- src/parsers/ini_parser.js
+++ src/parsers/ini_parser.js
@@ -1,20 +1,26 @@
 import ini from 'ini';
+import _ from 'lodash';
 
 const REGEX_FOR_NUMBER = /^-?[\d.]+(?:e-?\d+)?$/;
 
-const buildValue = (element) => {
-  if (!_.isObject(element)) {
-    return element;
-  }
+const objectParser = (data) => {
+  const result = Object.keys(data).reduce((acc, key) => {
+    const element = data[key];
 
-  // needed for rare case with ini files
-  const transform = (item) => (REGEX_FOR_NUMBER.test(item) ? parseInt(item, 10) : item);
-  const keys = Object.keys(element);
-  return keys.reduce((acc, key) => ({ ...acc, [key]: transform(element[key]) }), {});
+    if (_.isObject(element)) {
+      return { ...acc, [key]: objectParser(element) };
+    }
+
+    // rare case in json file ini parser parse number as string
+    const value = REGEX_FOR_NUMBER.test(element) ? parseInt(element, 10) : element;
+    return { ...acc, [key]: value }
+  }, {});
+
+  return result;
 };
 
 export default (data) => {
   const parsedData = ini.parse(data);
 
-  console.log(parsedData);
+  return objectParser(parsedData);
 }
