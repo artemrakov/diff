26259a5f55d45cfd8af169bec1e3160a1d2f4e69
Artem Rakov <scoff1k@gmail.com>
2020-03-16 15:12:54 -0400

    переделал абстракцию после видоса кирилла

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -2,20 +2,9 @@ import _ from 'lodash';
 
 const REGEX_FOR_NUMBER = /^-?[\d.]+(?:e-?\d+)?$/;
 
-const buildValue = (element) => {
-  if (!_.isObject(element)) {
-    return element;
-  }
-
-  // needed for rare case with ini files
-  const transform = (item) => (REGEX_FOR_NUMBER.test(item) ? parseInt(item, 10) : item);
-  const keys = Object.keys(element);
-  return keys.reduce((acc, key) => ({ ...acc, [key]: transform(element[key]) }), {});
-};
-
 const stringifyHelper = (nodes) => {
   const presentNode = {
-    added: (node) => ({ key: node.key, type: node.type, value: buildValue(node.value) }),
+    added: (node) => ({ key: node.key, type: node.type, value: node.value}),
     removed: (node) => ({ key: node.key, type: node.type }),
     unchanged: (node) => ({ key: node.key, type: node.type, value: node.value }),
     changed: (node) => ({
--- src/parsers/ini_parser.js
+++ src/parsers/ini_parser.js
@@ -1,3 +1,20 @@
 import ini from 'ini';
 
-export default ini.parse;
+const REGEX_FOR_NUMBER = /^-?[\d.]+(?:e-?\d+)?$/;
+
+const buildValue = (element) => {
+  if (!_.isObject(element)) {
+    return element;
+  }
+
+  // needed for rare case with ini files
+  const transform = (item) => (REGEX_FOR_NUMBER.test(item) ? parseInt(item, 10) : item);
+  const keys = Object.keys(element);
+  return keys.reduce((acc, key) => ({ ...acc, [key]: transform(element[key]) }), {});
+};
+
+export default (data) => {
+  const parsedData = ini.parse(data);
+
+  console.log(parsedData);
+}
